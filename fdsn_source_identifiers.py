0273f4337197cbc27b50d88c14533750152b9a47
chad-iris <chad.trabant@iris.edu>
2020-10-09 17:20:03 -0700

    Simplify conversion in fdsn_source_identifiers.py to be SEED specific, with added validation

--- fdsn_source_identifiers.py
+++ fdsn_source_identifiers.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python3
 
-"""This module contains the SourceID class for supporting FDSN Source Identifiers
+"""This module contains a SourceID class for FDSN Source Identifiers
 
 http://docs.fdsn.org/projects/source-identifiers
 """
 
 import sys
 import re
+from enum import Enum
+
+# A regex to validate Source Identifiers
+sourceid_re = re.compile('^FDSN:'              # Required prefix
+                         '([A-Z0-9]{1,8})'     # Required network: upper-alphanumeric
+                         '(_([-A-Z0-9]{1,8})'  # Optional non-empty station: upper-alphanumeric and dash
+                         '(_([-A-Z0-9]{0,8})'  # Optional possibly-empty location: upper-alphanumeric and dash
+                         '(_([A-Z0-9]*_[A-Z0-9]+_[A-Z0-9]*)' # Optional channel with non-empty source: upper-alphanumeric
+                         ')?)?)?$')            # Nested hierarchy of optional codes
+
+# Regex patterns to validate SEED-compatible codes
+network_seed_re = re.compile('^[A-Z0-9]{1,2}$')
+station_seed_re = re.compile('^[A-Z0-9]{1,5}$')
+location_seed_re = re.compile('^[A-Z0-9]{0,2}$')
+channel_seed_re = re.compile('^[A-Z0-9]{3,3}$')
+channel_seed_compat_re = re.compile('^[A-Z0-9]_[A-Z0-9]_[A-Z0-9]$')
 
 class SourceID(object):
     """Class for FDSN Source Identifiers: http://docs.fdsn.org/projects/source-identifiers
 
-    Methods support conversion to and from SEED network, station, location, and channel codes.
+    Methods support conversion to and from SEED and extended network, station, location, and channel codes.
 
     Validation of source identifiers and individual codes is included.
     """
@@ -23,7 +39,7 @@ class SourceID(object):
             if self.is_valid(sourceid):
                 self.sourceid = sourceid
             else:
-                raise Exception(f'Source Identifier is not recognized: {sourceid}')
+                raise Exception(f'Invalid Source Identifier: {sourceid}')
 
     def __str__(self):
         return f'{self.sourceid}'
@@ -34,87 +50,90 @@ class SourceID(object):
     def is_valid(self, sourceid=None):
         """Verify Source Identifier is a valid form"""
 
-        matchid = sourceid if sourceid is not None else self.sourceid
+        return True if sourceid_re.match(sourceid or self.sourceid) else False
 
-        # A regex to match Source Identifiers
-        sidre = re.compile('^FDSN:'              # Required prefix
-                           '([A-Z0-9]{1,8})'     # Required network: upper-alphanumeric
-                           '(_([-A-Z0-9]{1,8})'  # Optional non-empty station: upper-alphanumeric and dash
-                           '(_([-A-Z0-9]{0,8})'  # Optional possibly-empty location: upper-alphanumeric and dash
-                           '(_([A-Z0-9]*_[A-Z0-9]+_[A-Z0-9]*)' # Optional channel with non-empty source: upper-alphanumeric
-                           ')?)?)?$')            # Nested hierarchy of optional codes
-
-        return True if sidre.match(matchid) else False
-
-    @staticmethod
-    def from_nslc(network=None, station=None, location=None, channel=None, validate=True):
+    @classmethod
+    def from_seed(cls, network=None, station=None, location=None, channel=None, validate=True):
         """Generate a new SourceID constructed from individual SEED codes
 
         Convert SEED network, station, location, channel fields to a Source Identifier
         of the form: FDSN:NET_STA_LOC_CHAN, where CHAN="BAND_SOURCE_SUBSOURCE"
 
-        If the channel is stricly SEED (3 [A-Z0-9] characters) it will be converted to
-        the extended form of "BAND_SOURCE_SUBSOURCE".
+        The SEED channel is converted to the extended form of "BAND_SOURCE_SUBSOURCE".
         """
         if network is None:
             raise Exception(f'Network code is a minimum requirement')
 
         # Check for valid SEED and extended code contents
         if validate:
-            # Alphanumeric characters
-            if not re.match('^[A-Z0-9]{1,2}$', network):
-                raise Exception(f"Invalid network code:'{network}'")
+            errs = []
 
-            # Alphanumeric and dash
-            if station and not re.match('^[-A-Z0-9]{1,5}$', station):
-                raise Exception(f"Invalid station code:'{station}'")
+            if not network_seed_re.match(network):
+                errs.append(f"Invalid SEED network code:'{network}'")
 
-            # Alphanumeric and dash
-            if location and not re.match('^[-A-Z0-9]{0,2}$', location):
-                raise Exception(f"Invalid location code:'{location}'")
+            if station and not station_seed_re.match(station):
+                errs.append(f"Invalid SEED station code:'{station}'")
 
-            # Alphanumeric, either an extended code with underscores or exactly 3-characters
-            if channel and (not re.match('^[A-Z0-9]*_[A-Z0-9]+_[A-Z0-9]*$', channel) and
-                            not re.match('^[A-Z0-9]{3,3}$', channel)):
-                raise Exception(f"Invalid channel code:'{channel}'")
+            if location and not location_seed_re.match(location):
+                errs.append(f"Invalid SEED location code:'{location}'")
 
-        # Create an extended channel (band_source_subsource) from a stricly-SEED channel,
-        # identified when exactly 3 of [A-Z0-9] characters.
-        if channel is not None and re.match('^[A-Z0-9]{3,3}$', channel):
+            if channel and not channel_seed_re.match(channel):
+                errs.append(f"Invalid SEED channel code:'{channel}'")
+
+            if errs:
+                raise ValueError('\n'.join(errs))
+
+        # Create an extended channel (band_source_subsource) from SEED channel
+        if channel:
             channel = f'{channel[0]}_{channel[1]}_{channel[2]}'
 
         # Generate Source Identifier at appropriate level
         if station is None and location is None and channel is None:
-            return SourceID(f'FDSN:{network}')
+            return cls(f'FDSN:{network}')
         elif location is None and channel is None:
-            return SourceID(f'FDSN:{network}_{station}')
+            return cls(f'FDSN:{network}_{station}')
         elif channel is None:
-            return SourceID(f'FDSN:{network}_{station}_{location}')
+            return cls(f'FDSN:{network}_{station}_{location}')
         else:
-            return SourceID(f'FDSN:{network}_{station}_{location}_{channel}')
+            return cls(f'FDSN:{network}_{station}_{location}_{channel}')
 
-    def to_nslc(self, sourceid=None):
+    def to_seed(self, validate=True):
         """Split a Source Identifier into SEED network, station, location, channel codes
 
-        An extended channel the form "BAND_SOURCE_SUBSOURCE" is collapsed to SEED channel codes
-        when possible, i.e. when exactly 3 of [A-Z0-9] characters (with underscore separators).
+        An extended channel the form "BAND_SOURCE_SUBSOURCE" is collapsed to SEED channel codes.
         """
 
-        splitid = sourceid if sourceid is not None else self.sourceid
+        if self.sourceid is None:
+            raise Exception(f'Source Identifier is not available')
 
-        if splitid is None:
-            raise Exception(f'Source Identifier is specified')
-
-        if not self.is_valid(splitid):
-            raise Exception(f'Source Identifier is not recognized: {splitid}')
+        if not self.is_valid():
+            raise Exception(f'Invalid Source Identifier: {sourceid}')
 
         # Split into 4 codes and skip "FDSN:" prefix
-        network, station, location, channel, *_ = splitid[5:].split('_', maxsplit=3) + [None] * 4
+        network, station, location, channel, *_ = self.sourceid[5:].split('_', maxsplit=3) + [None] * 4
 
-        # Collapse an extended channel (band_source_subsource) to a stricly-SEED channel
-        # when possible, i.e. when exactly 3 of [A-Z0-9] characters (with underscore separators).
-        if channel is not None and re.match('^[A-Z0-9]_[A-Z0-9]_[A-Z0-9]$', channel):
+        # Collapse extended channel (band_source_subsource) to a SEED channel
+        if channel and channel_seed_compat_re.match(channel):
             channel = f'{channel[0]}{channel[2]}{channel[4]}'
+
+        # Enforce SEED code length limits
+        if validate:
+            errs = []
+
+            if not network_seed_re.match(network):
+                errs.append(f"Invalid SEED network code:'{network}'")
+
+            if station and not station_seed_re.match(station):
+                errs.append(f"Invalid SEED station code:'{station}'")
+
+            if location and not location_seed_re.match(location):
+                errs.append(f"Invalid SEED location code:'{location}'")
+
+            if channel and not channel_seed_re.match(channel):
+                errs.append(f"Invalid SEED channel code:'{channel}'")
+
+            if errs:
+                raise ValueError('\n'.join(errs))
 
         return [network, station, location, channel]
 
@@ -136,8 +155,6 @@ if __name__ == "__main__":
                                        {os.path.basename(sys.argv[0])} FDSN:XX_STA
                                        {os.path.basename(sys.argv[0])} FDSN:XX
                                        {os.path.basename(sys.argv[0])} XX STA LO LHZ
-                                       {os.path.basename(sys.argv[0])} XX STA LO L_H_Z
-                                       {os.path.basename(sys.argv[0])} XX STA LO LL_HH_ZZ
                                        {os.path.basename(sys.argv[0])} XX STA LO
                                        {os.path.basename(sys.argv[0])} XX STA
                                        {os.path.basename(sys.argv[0])} XX
@@ -156,7 +173,7 @@ if __name__ == "__main__":
             print (f'Generating SEED network, station, location, channel from SourceID {sid}')
 
         try:
-            network, station, location, channel, *_ = SourceID(sid).to_nslc() + [None] * 4
+            network, station, location, channel, *_ = SourceID(sid).to_seed() + [None] * 4
         except Exception as e:
             print(e)
             sys.exit(1)
@@ -169,7 +186,7 @@ if __name__ == "__main__":
                    f" Location: '{location}'" if location else '',
                    f" Channel: '{channel}'" if channel else ''))
 
-    # Otherwise individual codes are provided, convert to SourceID
+    # Otherwise individual SEED codes are provided, convert to SourceID
     elif len(args.inputcodes) > 0:
         network, station, location, channel, *_ = args.inputcodes + [None] * 4
 
@@ -177,12 +194,17 @@ if __name__ == "__main__":
             print (f'Generating SourceID from {network}, {station}, {location}, {channel}')
 
         try:
-            sid = SourceID.from_nslc(network, station, location, channel)
+            sid = SourceID.from_seed(network, station, location, channel)
         except Exception as e:
             print(e)
             sys.exit(1)
 
-        print (f"Input Network: '{network}', Station: '{station}', Location: '{location}', Channel: '{channel}'")
+        print ("Input {}{}{}{}".
+               format(
+                   f"Network: '{network}'",
+                   f" Station: '{station}'" if station else '',
+                   f" Location: '{location}'" if location else '',
+                   f" Channel: '{channel}'" if channel else ''))
         print (f"=> SourceID: '{sid}'")
 
     else:
